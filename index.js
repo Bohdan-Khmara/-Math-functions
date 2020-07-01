9529ca8b7f9c0d60bbf581b840005d19c68bd2b0
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-01 19:58:18 +0300

    Math function. Take the square root. Task 5

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /**
- * Math function. Take the square root. Task 4
- * @type {number}
+ * Math function. Take the square root. Task 5
+ * @type {number[]}
  */
-let result = Math.sqrt(245);
-alert(result);
+let array = [4, 2, 5, 19, 13, 0, 10];
+let inCube = [];
+let sumInCube = 0;
+let sqrtInCube = 0;
+
+for (let i = 0; i < array.length; i++) {
+    inCube.push(Math.pow(array[i], 3));
+}
+
+for (let j = 0; j < inCube.length; j++) {
+    sumInCube += inCube[j];
+    sqrtInCube = Math.sqrt(sumInCube);
+}
+console.log(sqrtInCube);
